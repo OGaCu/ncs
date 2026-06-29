e56d20569d59fd422dc60ef0dfbca932ce982468
Gia Yang <98694836+OGaCu@users.noreply.github.com>
2026-06-29 13:59:26 -0400

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # sliding window
+
+        left = 0
+        # window = {} # map key: char, value: freq
+        from collections import Counter
+        window = Counter()
+        longest = 0
+        right = 0
+
+        while right < len(s):
+            window[s[right]] += 1
+            right += 1
+            # out of k replacement then shrink window
+            while (right - left) - window.most_common(1)[0][1] > k: # max char freq
+                # remove the window[left] from map
+                window[s[left]] -= 1
+                left += 1
+        
+            longest = max(longest, right - left)
+        return longest
