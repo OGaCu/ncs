dbc1688c60db4e347675c0b06649aa2dfe892dc3
Gia Yang <98694836+OGaCu@users.noreply.github.com>
2026-07-09 12:55:09 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # O(V + E) where V is numCourses, E is len(prereq)
+        # DFS
+        prereqMap = {i:[] for i in range(numCourses)}
+
+        for course, prereq in prerequisites:
+            prereqMap[course].append(prereq)
+
+        visitSet = set() # return false bcuz loop
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if prereqMap[crs] == []:
+                return True
+            
+            visitSet.add(crs)
+            for pre in prereqMap[crs]:
+                if not dfs(pre): return False
+
+            visitSet.remove(crs)
+            prereqMap[crs] = []
+            return True
+        
+        for crs in range(numCourses): # need to call on every node to cover disconnected component
+            if not dfs(crs):
+                return False
+
+        return True
